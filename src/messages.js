e4cefa817da00192fd4991f2ba4ce9a6ce0ab50b
M Lokesh Gupta <mogalapallilokesh@gmail.com>
2025-08-10 17:27:25 +0530

    bday wishes repo

--- src/messages.js
+++ src/messages.js
@@ -2,39 +2,61 @@ const Messages = [
   {
     id: "1",
     start: new Date().getTime(),
-    message: "Hi, there! 👋",
+    message: "Knock knock 👀 ",
     delay: 0,
   },
   {
     id: "2",
     start: new Date().getTime() + 2200,
     message:
-      "So you have finally grown one year older but you have the brains of a child 😂",
+      "It’s me, Medha!",
     delay: 2200,
   },
   {
     id: "3",
     start: new Date().getTime() + 4200,
-    message: "What would you like for a gift... some meme's😁?",
+    message: "Here to sprinkle some birthday magic ✨",
     delay: 4200,
   },
   {
     id: "4",
     start: new Date().getTime() + 6200,
-    message: "Anyways keeping that aside, a very happy birthday to you 🎂",
+    message: "And maybe a little chaos too 😏",
     delay: 6200,
   },
   {
     id: "5",
     start: new Date().getTime() + 8200,
-    message: "Have a blast! ✨",
+    message: "Because life’s too short to be boring!",
     delay: 8200,
   },
   {
     id: "6",
     start: new Date().getTime() + 10200,
-    message: "Lokesh. 👨‍💻",
+    message: "You deserve hugs that feel like home,",
     delay: 10200,
+  },
+  {
+    id: "7",
+    start: new Date().getTime() + 12200,
+    message: "Moments that feel like sunshine,",
+    delay: 12200,
+  },
+  {
+    id: "8",
+    start: new Date().getTime() + 14200,
+    message: "And people who see how amazing you are 💖",
+    delay: 14200,
+  },{
+    id: "9",
+    start: new Date().getTime() + 16200,
+    message: "Happy Birthday, my dear 🌼",
+    delay: 16200,
+  },{
+    id: "10",
+    start: new Date().getTime() + 18200,
+    message: "– Always, Medha 💕",
+    delay: 18200,
   },
 ];
 
